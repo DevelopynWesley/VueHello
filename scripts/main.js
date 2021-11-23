7c9fa6b15dfab79410f6d43c9a134804e40f1bfa
DevelopynWesley <wesley.Barrett@developyn.com>
2021-11-23 15:32:48 +0000

    Begin Hello World

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        hello_world: 'Hello World!',
+        product: 'Socks',
+        description: 'Woolly',
+        image: './images/greensock.png',
+        details:["80% Cotton","20% Polyester","Unisex"],
+        hellos:["Hello World"]
+
+        }
+    
+
+
+,
+
+methods:{
+
+say_hello: function() {
+    this.hellos.push(this.hello_world);
+}
+
+
+}
+
+
+})
+
